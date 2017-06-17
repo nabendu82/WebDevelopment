58b1f5489b0c860713788aa502ab142be4f5b345
Nabendu <nabendu.biswas@gmail.com>
2017-06-17 18:31:57 +0530

    javaScript

---
+++ JSdemo.js
@@ -0,0 +1,3 @@
+var userName = prompt("What is your Name?");
+alert("Nice to mee you " + userName);
+cosole.log("ALso great to meet you" + userName);
---
+++ JavaScriptDemo/script.js
@@ -0,0 +1,3 @@
+var userName = prompt("What is your Name?");
+alert("Nice to mee you " + userName);
+console.log("ALso great to meet you" + userName);
