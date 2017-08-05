732e71273417d11a2433db875e40dce66827c673
Nabendu <nabendu.biswas@gmail.com>
2017-08-05 10:52:40 -0700

    FCCinventoryUpdate

--- FreecodeCamp/AdvancedAlgorithms/inventoryUpdate.js
+++ FreecodeCamp/AdvancedAlgorithms/inventoryUpdate.js
@@ -1,7 +1,25 @@
 //My Solution
 function updateInventory(arr1, arr2) {
-    // All inventory must be accounted for or you're fired!
-    return arr1;
+    arr2.forEach(function(array2){
+        arr1.forEach(function(array1){
+           if(array1[1] === array2[1])
+                array1[0] += array2[0];
+        });
+
+        var flattened = arr1.reduce(function(a, b) {
+          return a.concat(b);
+        }, []);
+        
+        if(flattened.indexOf(array2[1]) === -1)
+            arr1.push(array2);
+
+
+    });
+    return arr1.sort(function(a,b) { //sorting alphabatically
+        if (a[1] < b[1]) return -1;
+        else if (a[1] > b[1]) return 1;
+        return 0;
+    });
 }
 
 // Example inventory lists
