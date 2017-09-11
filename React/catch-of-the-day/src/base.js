6161731c9f92b6ebbcca9c0f89835c65fa2e41e7
nabendu82 <nabendu.biswas@gmail.com>
2017-09-11 10:13:37 +0530

    ReactAPPcomplete

---
+++ React/catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	   apiKey: "AIzaSyDlxWPHDzPjng5J1W4v5u-VqNyd7BYrzAQ",
+	   authDomain: "catch-of-the-day-nabendu.firebaseapp.com",
+	   databaseURL: "https://catch-of-the-day-nabendu.firebaseio.com",
+});
+
+export default base;
